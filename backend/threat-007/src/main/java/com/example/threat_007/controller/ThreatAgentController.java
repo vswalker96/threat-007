9aaf2cf93e18dfc34c6c5bef20a9b147a5e9abc6
Victoria <54386697+vswalker96@users.noreply.github.com>
2024-09-30 15:22:05 -0400

    Update ThreatAgentController.java

--- backend/threat-007/src/main/java/com/example/threat_007/controller/ThreatAgentController.java
+++ backend/threat-007/src/main/java/com/example/threat_007/controller/ThreatAgentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://172.18.0.4:4200", "http://138.197.3.104:4200", "*"})
 @RestController
 public class ThreatAgentController {
 
