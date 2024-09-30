c611142795f92b22913c9e80aeaa874aa9520322
louarnold3 <louislarnold93@gmail.com>
2024-09-30 14:20:24 -0400

    more docker updates

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Replace placeholder with the actual port for Nginx
-sed -i "s/listen 80;/listen $PORT;/" /etc/nginx/nginx.conf
-
-# Use $PORT for Nginx, a unique port for Spring Boot, and a different port for Uvicorn (FastAPI)
-uvicorn threat_engine:app --host 0.0.0.0 --port 8000 &  # FastAPI on port 8000
-java -jar /app/backend.jar --server.port=8080 --spring.profiles.active=dev &  # Spring Boot on port 8080
-nginx -g 'daemon off;'
