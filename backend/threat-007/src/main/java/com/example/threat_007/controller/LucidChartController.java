8e0756f3f64153f64b31b479419a1b66dca15792
Victoria Walker <victoria.walker@vivifysolutions.net>
2024-09-30 23:17:13 -0400

    Update backend back url

--- backend/threat-007/src/main/java/com/example/threat_007/controller/LucidChartController.java
+++ backend/threat-007/src/main/java/com/example/threat_007/controller/LucidChartController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 
+@RequestMapping("/api")
 @RestController
 public class LucidChartController {
 
--- frontend/threat-007/src/app/service/LucidChartService/lucid-chart.service.ts
+++ frontend/threat-007/src/app/service/LucidChartService/lucid-chart.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class LucidChartService {
   
-  private apiUrl = 'http://backend:8080';
+  private apiUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
